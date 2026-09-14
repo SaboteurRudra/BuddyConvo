efabe32a30e078474e4697e4d76ee3f1f1039472
SaboteurRudra <rudranshsinghfbd@gmail.com>
2026-09-14 22:40:52 +0530

    Update User.java

--- eclipse_jaava-workspace/BuddyConvo/src/main/java/BuddyConvo/User.java
+++ eclipse_jaava-workspace/BuddyConvo/src/main/java/BuddyConvo/User.java
@@ -1,41 +1,59 @@
 package BuddyConvo;
 
 public class User {
-	private int id;
-	private String username;
-	private String password;
-	private String status;
-	public User() {
-	}
-	public User(int id, String username, String password, String status) {
-		this.id = id;
-		this.username = username;
-		this.password = password;
-		this.status = status;
-	}
-	public int getId() {
-		return id;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public String getStatus() {
-		return status;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public void setStatus(String status) {
-		this.status = status;
-	}
 
+    private int id;
+    private String name;
+    private String email;
+    private String username;
+    private String password;
+    private String status;
+
+    public User() {
+    }
+    public User(int id, String name, String email,
+                String username, String password, String status) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.status = status;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
