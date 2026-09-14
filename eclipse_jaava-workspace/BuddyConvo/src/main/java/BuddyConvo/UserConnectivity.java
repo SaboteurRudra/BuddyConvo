45a83a74420f9dc73cf6479396141fd84a2db256
SaboteurRudra <rudranshsinghfbd@gmail.com>
2026-09-14 22:41:34 +0530

    Update UserConnectivity.java

--- eclipse_jaava-workspace/BuddyConvo/src/main/java/BuddyConvo/UserConnectivity.java
+++ eclipse_jaava-workspace/BuddyConvo/src/main/java/BuddyConvo/UserConnectivity.java
@@ -1,65 +1,36 @@
-package BuddyConvo;
-import BuddyConvo.User;
-import java.sql.*;
+public User login(String username, String password) {
 
-public class UserConnectivity {
-	
-	public boolean register(String name, String email,
-            String username, String password) {
+    String sql = "SELECT * FROM users WHERE username = ? AND password = ?";
 
-String sql = "INSERT INTO users (name, email, username, password) " +
-     "VALUES (?, ?, ?, ?)";
+    try (Connection con = DBConnection.getConnection();
+         PreparedStatement ps = con.prepareStatement(sql)) {
 
-try (Connection con = DBConnection.getConnection();
-PreparedStatement ps = con.prepareStatement(sql)) {
+        ps.setString(1, username);
+        ps.setString(2, password);
 
-ps.setString(1, name);
-ps.setString(2, email);
-ps.setString(3, username);
-ps.setString(4, password);
+        ResultSet rs = ps.executeQuery();
 
-ps.executeUpdate();
+        if (rs.next()) {
 
-return true;
+            User user = new User();
 
-} catch (Exception e) {
-System.out.println("REGISTRATION ERROR:");
-e.printStackTrace();
-return false;
+            user.setId(rs.getInt("id"));
+            user.setName(rs.getString("name"));
+            user.setEmail(rs.getString("email"));
+            user.setUsername(rs.getString("username"));
+            user.setPassword(rs.getString("password"));
+            user.setStatus(rs.getString("status"));
+
+            return user;
+        }
+
+    } catch (Exception e) {
+
+        System.out.println("LOGIN ERROR:");
+        e.printStackTrace();
+    }
+
+    return null;
 }
-}
-	public User login(String username,
-			          String password) {
-		String sql = "SELECT * from users where username =? AND password =?";
-		try (Connection con = DBConnection.getConnection();
-			 PreparedStatement ps = con.prepareStatement(sql)){
-			ps.setString(1, username);
-			ps.setString(2, password);
-			ResultSet rs = ps.executeQuery();
-			if(rs.next()) {
-				return new User(
-						rs.getInt("id"),
-						rs.getString("username"),
-						rs.getString("password"),
-						rs.getString("status")
-				);
-			}
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
-		return null;
-	}
-	public void updateStatus(int userId,
-			                 String status) {
-		String sql = "UPDATE users SET status=? WHERE id=?";
-		try(Connection con = DBConnection.getConnection();
-			PreparedStatement ps = con.prepareStatement(sql)){
-		   ps.setString(1, status);
-		   ps.setInt(2, userId);
-		   ps.executeUpdate();
-		}catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
-	}
 
-}
+		
