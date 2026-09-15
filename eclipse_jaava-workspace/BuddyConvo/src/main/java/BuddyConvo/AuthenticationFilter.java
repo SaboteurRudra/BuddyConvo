c9bb03810e5a06d0df3105def6ab191640997c92
SaboteurRudra <rudranshsinghfbd@gmail.com>
2026-09-15 08:54:06 +0530

    Update AuthenticationFilter.java

--- eclipse_jaava-workspace/BuddyConvo/src/main/java/BuddyConvo/AuthenticationFilter.java
+++ eclipse_jaava-workspace/BuddyConvo/src/main/java/BuddyConvo/AuthenticationFilter.java
@@ -12,7 +12,7 @@ import jakarta.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebFilter("/*")
-public class AuthFilter implements Filter {
+public class AuthenticationFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request,
